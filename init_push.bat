1382f4cb5cfc9214f42e1488193e20b8965dac94
formateur fou <me@ici_ou_labas.fr>
2018-04-27 11:05:15 +0200

    version initiale

---
+++ init_push.bat
@@ -0,0 +1,10 @@
+cd /d "%~dp0"
+git init
+git add *
+git commit -a -m "version initiale"
+git remote add gitHubOriginTpSpring https://didier-tp:pwd007!@github.com/didier-tp/m2idef_tpSpring.git
+git push -u gitHubOriginTpSpring master
+pause
+
+REM open with text editor
+REM opne with system editor
