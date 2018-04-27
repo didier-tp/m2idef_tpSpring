89c6b0361ae262be7089cf5ae5405822b33cd5c7
formateur fou <me@ici_ou_labas.fr>
2018-04-27 11:05:55 +0200

    nouvelle version

--- init_push.bat
+++ init_push.bat
@@ -2,7 +2,7 @@ cd /d "%~dp0"
 git init
 git add *
 git commit -a -m "version initiale"
-git remote add gitHubOriginTpSpring https://didier-tp:pwd007!@github.com/didier-tp/m2idef_tpSpring.git
+git remote add gitHubOriginTpSpring https://didier-tp:password!@github.com/didier-tp/m2idef_tpSpring.git
 git push -u gitHubOriginTpSpring master
 pause
 
